0d08576488716f480a3ae51aad98339c09ece3b9
jzm284 <jzm284@utexas.edu>
2024-08-18 18:06:13 -0500

    refactored logic to better follow SRP

--- create_maze.py
+++ create_maze.py
@@ -3,6 +3,8 @@ import pygame
 
 # right, down, left, up
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+LIGHT_BLUE = (153, 204, 255)
+WHITE = (255, 255, 255)
 
 def get_grid(width: int, height: int):
     """
@@ -36,38 +38,22 @@ def get_grid(width: int, height: int):
     
     return globalEdges
 
-def draw_maze(width, height, edges):
-    """
-    Creates the maze of width x height dimensions using Kruskal's algorithm.
-    Visually displays process in Pygame window.
-
-    Args:
-        width (int): width (highest x value + 1) of the maze
-        height (int): height (highest y value + 1) of the maze
-        edges (int): list of edges between nodes in the maze
-
-    Returns:
-        : 1 on successful completion, else 0
-    """
-    pygame.init()
-    screen = pygame.display.set_mode((width * 40, height * 40))
-    clock = pygame.time.Clock()
-    screen.fill((153, 204, 255))
-
+def draw_grid(width, height, screen):
     for i in range(width):
         for j in range(height):
             x = i * 40
             y = j * 40
 
-            pygame.draw.line(screen, (255, 255, 255), (x, y), (x + 40, y), 1)  # Top wall
-            pygame.draw.line(screen, (255, 255, 255), (x, y), (x, y + 40), 1)  # Left wall
-            pygame.draw.line(screen, (255, 255, 255), (x + 40, y), (x + 40, y + 40), 1)  # Right wall
-            pygame.draw.line(screen, (255, 255, 255), (x, y + 40), (x + 40, y + 40), 1)  # Bottom wall
-    
+            pygame.draw.line(screen, WHITE, (x, y), (x + 40, y), 1)  # Top wall
+            pygame.draw.line(screen, WHITE, (x, y), (x, y + 40), 1)  # Left wall
+            pygame.draw.line(screen, WHITE, (x + 40, y), (x + 40, y + 40), 1)  # Right wall
+            pygame.draw.line(screen, WHITE, (x, y + 40), (x + 40, y + 40), 1)  # Bottom wall
     pygame.display.flip()
+
+def draw_maze(width, height, screen, clock, edges):
     n = width * height
     minSpanningTree = []
-
+    
     while len(minSpanningTree) < n - 1:
         edge = edges.pop(0)
         if edge.node1.group != edge.node2.group:
@@ -79,42 +65,56 @@ def draw_maze(width, height, edges):
             x1, y1 = node1.x, node1.y
             x2, y2 = node2.x, node2.y
 
-
             if x1 == x2 and y1 == y2 - 1:  # node2 is below node1
-                pygame.draw.line(screen, (153, 204, 255), (node1.x * 40, node1.y * 40 + 40), (node1.x * 40 + 40, node1.y * 40 + 40), 1)
-                print("removed bottom wall")
+                pygame.draw.line(screen, LIGHT_BLUE, (node1.x * 40, node1.y * 40 + 40), (node1.x * 40 + 40, node1.y * 40 + 40), 1)
             elif x1 == x2 and y1 == y2 + 1:  # node2 is above node1
-                pygame.draw.line(screen, (153, 204, 255), (node2.x * 40, node2.y * 40 + 40), (node2.x * 40 + 40, node2.y * 40 + 40), 1)
-                print("removed top wall")
+                pygame.draw.line(screen, LIGHT_BLUE, (node2.x * 40, node2.y * 40 + 40), (node2.x * 40 + 40, node2.y * 40 + 40), 1)
             elif x1 == x2 - 1 and y1 == y2:  # node2 is to the right of node1
-                pygame.draw.line(screen, (153, 204, 255), (node1.x * 40 + 40, node1.y * 40), (node1.x * 40 + 40, node1.y * 40 + 40), 1)
-                print("removed right wall")
+                pygame.draw.line(screen, LIGHT_BLUE, (node1.x * 40 + 40, node1.y * 40), (node1.x * 40 + 40, node1.y * 40 + 40), 1)
             elif x1 == x2 + 1 and y1 == y2:  # node2 is to the left of node1
-                pygame.draw.line(screen, (153, 204, 255), (node2.x * 40 + 40, node2.y * 40), (node2.x * 40 + 40, node2.y * 40 + 40), 1)
-                print("removed left wall")
-
+                pygame.draw.line(screen, LIGHT_BLUE, (node2.x * 40 + 40, node2.y * 40), (node2.x * 40 + 40, node2.y * 40 + 40), 1)
 
             pygame.display.update()
-            clock.tick(100)
+            clock.tick(60)
         else:
             continue
-        print(str(edge))
+    return minSpanningTree
+    
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return 1
+def create_maze(width, height):
+    """
+    Creates the maze of width x height dimensions using Kruskal's algorithm.
+    Visually displays process in Pygame window.
 
+    Args:
+        width (int): width (highest x value + 1) of the maze
+        height (int): height (highest y value + 1) of the maze
+        edges (int): list of edges between nodes in the maze
+
+    Returns:
+        : the minimum spannign tree on successful completion, else None
+    """
+    pygame.init()
+    screen = pygame.display.set_mode((width * 40, height * 40))
+    clock = pygame.time.Clock()
+    screen.fill(LIGHT_BLUE)
+    
+    # Flag to avoid recreating the maze on every iteration
+    firstRun = True
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+        if firstRun:
+            firstRun = False
+            edges = get_grid(width, height)
+            draw_grid(width, height, screen)
+            maze = draw_maze(width, height, screen, clock, edges)
         clock.tick(60)
-
     pygame.quit()
-    return 1
+    
+    return maze
 
 class Node:
     def __init__(self, x, y):
@@ -146,7 +146,4 @@ class Edge:
         return f"{self.node1} -> {self.node2}"
 
 if __name__ == "__main__":
-    width = 20
-    height = 20
-    edges = get_grid(width, height)
-    draw_maze(width, height, edges)
+    create_maze(20, 20)
