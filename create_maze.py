7c3515f0ec9195a13369df22010ddabb8a3c1f1b
jzm284 <jzm284@utexas.edu>
2024-08-18 13:21:50 -0500

    created Node, Edge classes

--- create_maze.py
+++ create_maze.py
@@ -1,2 +1,32 @@
 import random
 import pygame
+
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.group = set()
+        self.group.add(self)
+    
+    def union(self, other):
+        self.group.update(other.group)
+        for node in other.group:
+            node.group = self.group
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+class Edge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+    
+    def __str__(self):
+        return f"{self.node1} -> {self.node2}"
+
