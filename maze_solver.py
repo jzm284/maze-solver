47baeaf7d408d247fff250e0dc795075e7105124
jzm284 <jzm284@utexas.edu>
2024-09-13 15:28:23 -0500

    brazilian jiu jitsu commit

--- maze_solver.py
+++ maze_solver.py
@@ -7,8 +7,20 @@ def a_star(screen, clock, maze, start, end):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        curr_node = None
+        for edge in maze:
+            if (edge.node1.x == start[0] and edge.node1.y == start[1]):
+                curr_node = edge.node1
+                break
+            elif (edge.node2.x == start[0] and edge.node2.y == start[1]):
+                curr_node = edge.node2
+                break
         
-    
+        if curr_node == None:
+            print("Error: Start node not found in maze")
+            return
+        
+        
     
     
     
